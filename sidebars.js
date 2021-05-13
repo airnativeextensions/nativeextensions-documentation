b90c2558f44d34c94c477ec4167aca3b2f6c1882
marchbold <marchbold@users.noreply.github.com>
2021-05-13 22:39:04 +1000

    Adverts sidebar update

--- sidebars.js
+++ sidebars.js
@@ -119,7 +119,8 @@ module.exports = {
                 "adverts/interstitials",
                 "adverts/rewarded-video-ads",
                 "adverts/native-ads",
-                "adverts/targeting"
+                "adverts/targeting",
+                "adverts/test-ads"
             ]
         },
         { 
