48781d5d0bd90dbc0efd4517d440ab80e01c1e81
marchbold <marchbold@users.noreply.github.com>
2022-06-15 11:54:20 +1000

    add news article on accessing gdpr settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,151 +1,153 @@
 module.exports = {
-	title: "air native extensions",
-	tagline: "Documentation for distriqt's Native Extensions",
-	url: "https://docs.airnativeextensions.com",
-	baseUrl: "/",
-	// trailingSlash: true,
-	onBrokenLinks: "throw",
-	favicon: "img/favicon.ico",
-	organizationName: "airnativeextensions", // Usually your GitHub org/user name.
-	projectName: "nativeextensions-documentation", // Usually your repo name.
-	themeConfig: {
-		// announcementBar: {
-		//   id: 'development', // Any value that will identify this message.
-		//   content:
-		//     'This is the new airnativeextensions documentation site and is currently under development. <a href="https://github.com/airnativeextensions/nativeextensions-documentation/issues">Please let us know if you find any issues.</a>',
-		//   backgroundColor: '#880000', // Defaults to `#fff`.
-		//   textColor: '#FFF', // Defaults to `#000`.
-		//   isCloseable: false, // Defaults to `true`.
-		// },
+  title: "air native extensions",
+  tagline: "Documentation for distriqt's Native Extensions",
+  url: "https://docs.airnativeextensions.com",
+  baseUrl: "/",
+  // trailingSlash: true,
+  onBrokenLinks: "throw",
+  favicon: "img/favicon.ico",
+  organizationName: "airnativeextensions", // Usually your GitHub org/user name.
+  projectName: "nativeextensions-documentation", // Usually your repo name.
+  themeConfig: {
+    // announcementBar: {
+    //   id: 'development', // Any value that will identify this message.
+    //   content:
+    //     'This is the new airnativeextensions documentation site and is currently under development. <a href="https://github.com/airnativeextensions/nativeextensions-documentation/issues">Please let us know if you find any issues.</a>',
+    //   backgroundColor: '#880000', // Defaults to `#fff`.
+    //   textColor: '#FFF', // Defaults to `#000`.
+    //   isCloseable: false, // Defaults to `true`.
+    // },
 
-		image: "img/ane-icon-black.png",
-		colorMode: {
-			disableSwitch: false,
-			switchConfig: {
-				darkIcon: " ",
-				lightIcon: " ",
-			},
-		},
+    image: "img/ane-icon-black.png",
+    colorMode: {
+      disableSwitch: false,
+      switchConfig: {
+        darkIcon: " ",
+        lightIcon: " ",
+      },
+    },
 
-		prism: {
-			theme: require("prism-react-renderer/themes/github"),
-			darkTheme: require("prism-react-renderer/themes/vsDark"),
-			additionalLanguages: ["actionscript", "csharp"],
-			// defaultLanguage: 'actionscript'
-		},
-		algolia: {
-			appId: "WGX2M70X7C",
-			apiKey: "f971c0416d41b295925d2ce7350e386d",
-			indexName: "airnativeextensions",
-			placeholder: "Search...",
-			contextualSearch: false,
-			searchParameters: {
-				facetFilters: ['language:en'],
-			},
-		},
-		navbar: {
-			title: "AIR Native Extensions",
-			logo: {
-				alt: "AIR Native Extensions",
-				src: "img/ane-icon-black.png",
-				srcDark: "img/ane-icon-white.png",
-			},
-			items: [
-				{
-					to: "docs/",
-					activeBasePath: "docs",
-					label: "Docs",
-					position: "left",
-				},
-				{
-					to: "news/",
-					label: "News",
-					position: "left",
-				},
-			],
-		},
-		footer: {
-			style: "dark",
-			links: [
-				{
-					title: "Docs",
-					items: [
-						{
-							label: "tutorials",
-							to: "/docs/tutorials/",
-						},
-					],
-				},
-				{
-					title: "Community",
-					items: [
-						{
-							label: "twitter",
-							href: "https://twitter.com/distriqt",
-						},
-						{
-							label: "AIR discussions",
-							href: "https://github.com/Gamua/Adobe-Runtime-Support/discussions",
-						},
-						{
-							label: "starling",
-							href: "https://forum.starling-framework.org/",
-						},
-						{
-							label: "stack overflow",
-							href: "https://stackoverflow.com/questions/tagged/distriqt",
-						},
-					],
-				},
-				{
-					title: "More",
-					items: [
-						{
-							label: "news",
-							to: "news",
-						},
-						{
-							label: "privacy",
-							href: "https://airnativeextensions.com/privacy",
-						},
-						{
-							label: "license",
-							href: "https://airnativeextensions.com/license",
-						},
-					],
-				},
-			],
-			copyright: `<br/><a href="https://airnativeextensions.com">airnativeextensions.com</a> copyright © ${new Date().getFullYear()} <br/><br/><a href="https://distriqt.com"><img src="/img/logo.png" alt="distriqt" /></a>`,
-		},
-	},
-	presets: [
-		[
-			"@docusaurus/preset-classic",
-			{
-				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-					routeBasePath: "/docs",
-					// editUrl: 'https://github.com/airnativeextensions/nativeextensions-documentation/',
-				},
-				blog: {
-					blogDescription: "distriqt // Native Extension News",
-					showReadingTime: true,
-					routeBasePath: "news",
-					path: "./news",
-					feedOptions: {
-						type: "all",
-						copyright: `Copyright © ${new Date().getFullYear()} distriqt Pty Ltd`,
-					},
-					// editUrl: 'https://github.com/airnativeextensions/nativeextensions-documentation/edit/master/website/blog/',
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.css"),
-				},
-				googleAnalytics: {
-					trackingID: "UA-188619114-3",
-					anonymizeIP: true,
-				},
-			},
-		],
-	],
+    prism: {
+      theme: require("prism-react-renderer/themes/github"),
+      darkTheme: require("prism-react-renderer/themes/vsDark"),
+      additionalLanguages: ["actionscript", "csharp"],
+      // defaultLanguage: 'actionscript'
+    },
+    algolia: {
+      appId: "WGX2M70X7C",
+      apiKey: "f971c0416d41b295925d2ce7350e386d",
+      indexName: "airnativeextensions",
+      placeholder: "Search...",
+      contextualSearch: false,
+      searchParameters: {
+        facetFilters: ["language:en"],
+      },
+    },
+    navbar: {
+      title: "AIR Native Extensions",
+      logo: {
+        alt: "AIR Native Extensions",
+        src: "img/ane-icon-black.png",
+        srcDark: "img/ane-icon-white.png",
+      },
+      items: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+        },
+        {
+          to: "news/",
+          label: "News",
+          position: "left",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "tutorials",
+              to: "/docs/tutorials/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "twitter",
+              href: "https://twitter.com/distriqt",
+            },
+            {
+              label: "AIR discussions",
+              href: "https://github.com/Gamua/Adobe-Runtime-Support/discussions",
+            },
+            {
+              label: "starling",
+              href: "https://forum.starling-framework.org/",
+            },
+            {
+              label: "stack overflow",
+              href: "https://stackoverflow.com/questions/tagged/distriqt",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "news",
+              to: "news",
+            },
+            {
+              label: "privacy",
+              href: "https://airnativeextensions.com/privacy",
+            },
+            {
+              label: "license",
+              href: "https://airnativeextensions.com/license",
+            },
+          ],
+        },
+      ],
+      copyright: `<br/><a href="https://airnativeextensions.com">airnativeextensions.com</a> copyright © ${new Date().getFullYear()} <br/><br/><a href="https://distriqt.com"><img src="/img/logo.png" alt="distriqt" /></a>`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/docs",
+          // editUrl: 'https://github.com/airnativeextensions/nativeextensions-documentation/',
+        },
+        blog: {
+          blogDescription: "distriqt // Native Extension News",
+          showReadingTime: true,
+          blogSidebarTitle: "Latest News",
+          blogSidebarCount: 10,
+          routeBasePath: "news",
+          path: "./news",
+          feedOptions: {
+            type: "all",
+            copyright: `Copyright © ${new Date().getFullYear()} distriqt Pty Ltd`,
+          },
+          // editUrl: 'https://github.com/airnativeextensions/nativeextensions-documentation/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        googleAnalytics: {
+          trackingID: "UA-188619114-3",
+          anonymizeIP: true,
+        },
+      },
+    ],
+  ],
 };
---
+++ news/2022-06-15-accessing-gdpr-settings.md
@@ -0,0 +1,47 @@
+---
+title: Accessing GDPR Settings
+description: Accessing GDPR Transparency and Consent settings from the AIR SDK
+image: images/gdpr.png
+authors: [ marchbold ]
+tags: [ airsdk, gdpr, ane, advertising ]
+---
+
+At some point in your application development when you start to store and share data you will need to handle user GDPR settings. 
+
+:::info GDPR
+The General Data Protection Regulation (GDPR) is a European Union (EU) regulation that mandates how an organisation should handle personal data. 
+:::
+
+If you use personalised advertising or store user related data on your server then you will need to address GDPR concerns in your application.
+
+<!--truncate-->
+
+On mobile devices the GDPR settings are stored through the **IAB Europe Transparency & Consent Framework**. This framework ensures settings are stored in consistent places for developers to access and determine their appropriate behaviour for the user.
+
+These values are stored in [NSUserDefaults](https://developer.apple.com/documentation/foundation/nsuserdefaults#1664798?language=objc) on iOS and in [SharedPreferences](https://developer.android.com/training/data-storage/shared-preferences.html) on Android.
+
+With the AIR SDK we can access these values easily through the [Application extension](https://airnativeextensions.com/extension/com.distriqt.Application). The Application extension allows access to the NSUserDefaults and the SharedPreferences through the [`defaults`](https://docs.airnativeextensions.com/docs/application/defaults) functionality.
+
+Firstly, we set the `useSharedDefaults` flag to ensure we use the application's shared values. 
+
+```actionscript
+Application.service.defaults.useSharedDefaults = true;
+```
+
+> If we don't set this flag, then the values retrieved through the `defaults` functionality will be isolated from values set via other methods and you won't retrieve the correct TC data values. 
+
+Once you have set this flag you can retrieve any of the TC data from the framework by using the appropriate key. 
+
+For example:
+
+```actionscript
+var value:String = 
+    Application.service.defaults.
+        getString( "IABTCF_TCString" );
+```
+
+You can then use this value as required for your implementation of GDPR in your application.
+
+
+For a full list of the available keys and a description of the values and types see the [documentation here](https://github.com/InteractiveAdvertisingBureau/GDPR-Transparency-and-Consent-Framework/blob/master/TCFv2/IAB%20Tech%20Lab%20-%20CMP%20API%20v2.md#what-is-the-cmp-in-app-internal-structure-for-the-defined-api).
+
