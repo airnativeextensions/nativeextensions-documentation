6acab831425952e577b9ab527ab8aa88642a944c
Michael <marchbold@users.noreply.github.com>
2024-12-19 16:38:33 +1000

    speech v3.0.0

---
+++ docs/sidebars.js
@@ -0,0 +1,31 @@
+speech: [
+    { type: "ref", id: "index" },
+    { type: "doc", id: "speech/index" },
+    {
+      "Get Started": [
+        "speech/add-the-extension",
+        "speech/authorisation",
+      ],
+    },
+    {
+      Usage: [
+        "speech/texttospeech",
+        "speech/recognition",
+      ],
+    },
+    {
+      Other: [
+        { type: "doc", id: "speech/changelog" },
+        {
+          type: "link",
+          label: "asdocs",
+          href: "https://docs.airnativeextensions.com/asdocs/speech/",
+        },
+        {
+          type: "link",
+          label: "Contact Support",
+          href: "http://github.com/airnativeextensions/ANE-Speech/issues/new",
+        },
+      ],
+    },
+  ],
--- docs/speech/changelog.md
+++ docs/speech/changelog.md
@@ -1,3 +1,15 @@
+### 2024.12.19 [v3.0.0]
+
+```
+## Major update 
+
+In this update we have moved the extension to use the newer gradle dependencies process.
+
+### Updates 
+
+feat(android): move to gradle dependencies
+```
+
 ### 2023.10.19 [v2.0.1]
 
 ```
